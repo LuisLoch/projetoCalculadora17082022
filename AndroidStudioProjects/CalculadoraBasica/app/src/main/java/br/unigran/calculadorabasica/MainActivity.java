5ea796a4b3da83befa8b9f16aa02eee42a6ebd66
Luis <802.250@alunos.unigran.br>
2022-08-17 21:47:18 -0400

    segunda commit 17082022

--- AndroidStudioProjects/CalculadoraBasica/app/src/main/java/br/unigran/calculadorabasica/MainActivity.java
+++ AndroidStudioProjects/CalculadoraBasica/app/src/main/java/br/unigran/calculadorabasica/MainActivity.java
@@ -3,8 +3,12 @@ package br.unigran.calculadorabasica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_add, btn_sub;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
