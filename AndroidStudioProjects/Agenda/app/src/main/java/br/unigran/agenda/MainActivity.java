ad1d1af66f3286fbd6631fff0428c331b88efa84
Luis <802.250@alunos.unigran.br>
2022-08-24 20:58:41 -0400

    criando funcao salvar

--- AndroidStudioProjects/Agenda/app/src/main/java/br/unigran/agenda/MainActivity.java
+++ AndroidStudioProjects/Agenda/app/src/main/java/br/unigran/agenda/MainActivity.java
@@ -3,12 +3,31 @@ package br.unigran.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nome, numero;
+    List dados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nome = findViewById(R.id.nomeId);
+        numero = findViewById(R.id.numeroId);
+        dados = new ArrayList();
+    }
+
+    public void salvar(View view){
+        Contato contato = new Contato();
+        contato.setNome(nome.getText().toString());
+        contato.setTelefone(numero.getText().toString());
+        dados.add(contato);
+        Toast.makeText(this, "Salvo com sucesso!", Toast.LENGTH_SHORT).show();
     }
 }
